9e2148dc906ebc2a0c36e5b26ee0df07459a3a30
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-01-20 22:32:01 +0900

    Add some sources

---
+++ Controller.cpp
@@ -0,0 +1,32 @@
+//
+//  Controler.cpp
+//  level7
+//
+//  Created by Yume OOHAMA on 2015/01/20.
+//  Copyright (c) 2015年 Yume OOHAMA. All rights reserved.
+//
+
+#include "Controller.h"
+
+void Controller::update(){
+    
+    if (state_==OBJECT_DETECT) {
+        // objDetector -> detectObject();
+        // detectObjectの返り値がtrueならstate_をMOTION_DETECTにかえる
+        // state_ = MOTION_DETECT;
+        
+    }else if (state_==MOTION_DETECT){
+    
+        
+        
+    }else{ //state_ == ANIMATION
+        
+        
+    }
+    
+}
+
+void Controller::draw(){
+    // appから呼ぶ
+    return;
+}
---
+++ Controller.h
@@ -0,0 +1,35 @@
+//
+//  Controler.h
+//  level7
+//
+//  Created by Yume OOHAMA on 2015/01/20.
+//  Copyright (c) 2015年 Yume OOHAMA. All rights reserved.
+//
+
+#ifndef __level7__Controler__
+#define __level7__Controler__
+
+#include <stdio.h>
+
+class Controller{
+public:
+    Controller(){state_ = OBJECT_DETECT;}
+    void update();
+    void draw();
+    
+private:
+    
+    enum STATE {
+      OBJECT_DETECT,
+      MOTION_DETECT,
+      ANIMATION
+    };
+    
+    STATE state_;
+//   ObjectDetector *objDetector_;
+//   MotionDetector *motDetector_;
+    
+};
+
+#endif /* defined(__level7__Controler__) */
+
